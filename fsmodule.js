2c9ebdb857f5013d096b8282a873991c73b11229
onkarbhawar111 <onkarbhawar111@gmail.com>
2025-05-14 12:55:03 +0530

    14-5

---
+++ fsmodule.js
@@ -0,0 +1,45 @@
+import fs from 'fs'
+
+// fs.writeFileSync('message1.txt', 'Hello from Node.js !!!!!')
+
+// fs.writeFile('message.txt', "Asynchronous task completed...", (err)=>{
+//     if(err) throw err;
+// })
+
+// console.log('1')
+// fs.readFile('message111.txt', 'utf8', (err, data)=>{
+//     if(err) throw err;
+//     console.log(data)
+// } )
+// console.log('2')
+
+
+// console.log('1')
+// const data = fs.readFileSync('message.txt', 'utf8')
+// console.log(data)
+// console.log('2')
+
+
+
+// fs.appendFile('message11.txt', ' \nNew Line Added', (err)=>{
+//     if(err) throw err;
+//     console.log('file appended successfully.....')
+// })
+
+
+// fs.unlink('message1.txt',  (err)=>{
+//     if(err) throw err;
+//     console.log('File deleted....')
+// })
+// fs.mkdir('myFolder1', ()=>{})
+
+//    fs.readdir('./myFolder', (err, data)=>{
+//     if(err) throw err;
+//     console.log(data)
+//    }) 
+
+//    if(fs.existsSync('myFolder1/ind.html')){
+//     console.log("File Exists...")
+//    } else{
+//     console.log('File doesnot exist....')
+//    }
